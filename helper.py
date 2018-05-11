26c2f97be1fb80cce1ecba3e1c890e9cca8d232c
Miles Smith <themilessmith@gmail.com>
2018-05-11 00:27:14 -0700

    Forgot helper.py

---
+++ helper.py
@@ -0,0 +1,32 @@
+import os
+import sys 
+import pygame
+from math import *
+from pygame.locals import *
+from platform import system
+from datetime import datetime
+
+
+class Color:
+    Primary = (0, 153, 255)
+    Text = (0, 153, 255)
+    Background = (35, 35, 35)
+
+
+
+def text(skrn, text, pos=(0, 0), size=60, color=(255, 200, 100)):
+    sys_font = pygame.font.SysFont("Arial", int(size))
+    rendered = sys_font.render(str(text), 0, color)
+    skrn.blit(rendered, pos)
+
+
+pygame.init()
+pygame.mixer.init()
+# Windows is used to test and debug on my laptop for now, so we don't want it fullscreen
+#   Since that's apain in the ass to test on
+if system() == 'Windows':
+    skrn = pygame.display.set_mode((800, 480))
+else:
+    skrn = pygame.display.set_mode((800, 480), pygame.FULLSCREEN)
+
+SWIPE_TIME = 2
